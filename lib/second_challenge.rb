93ecf7704a3aa407759e8798a14023a1721a580e
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-18 23:48:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
-      :hero_friends
-      }, 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+      }, #montague
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      } #capulet
+  } #epic_tragedy
 
-  
 end
